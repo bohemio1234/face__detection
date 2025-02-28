cc7f883b01dc51ea9966ff9a3a9426b2b6f96c7b
bohemio1234 <bohemio1234@naver.com>
2025-02-28 18:13:27 +0900

    render

--- face-detection-server/server.js
+++ face-detection-server/server.js
@@ -14,49 +14,13 @@ dotenv.config();
 
 const db = knex({
   client: "pg",
-  connection: {
-    host: "127.0.0.1",
-    port: 5432,
-    user: "",
-    password: "",
-    database: "real-smart-brain",
-  },
+  connection: process.env.DATABASE_URL,
 });
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-const CLARIFAI_API_URL =
-  "https://api.clarifai.com/v2/models/face-detection/outputs";
-const CLARIFAI_PAT = "fdb75dd67a2742e8a87b45d496bb9065";
-
-app.post("/clarifai", async (req, res) => {
-  try {
-    const requestData = req.body;
-    const response = await fetch(CLARIFAI_API_URL, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: "Key " + CLARIFAI_PAT,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(requestData),
-    });
-
-    if (!response.ok) {
-      throw new Error("Clarifai API 요청 실패");
-    }
-
-    const data = await response.json();
-
-    res.json(data);
-  } catch (error) {
-    console.error("Clarifai API 요청 오류:", error);
-    res.status(500).json({ error: "Clarifai API요청 실패" });
-  }
-});
 
 app.post("/clarifai", (req, res) => {
   clarifai.handleClarifaiAPI(req, res);
